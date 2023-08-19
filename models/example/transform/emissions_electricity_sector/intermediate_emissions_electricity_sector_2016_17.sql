4888cbefbb9cfd65617046f1a3012f71aab612bc
Hari Krishna <hariprasadk@protonmail.com>
2023-08-19 19:26:36 +0000

    Added safe cast for scope

--- models/example/transform/emissions_electricity_sector/intermediate_emissions_electricity_sector_2016_17.sql
+++ models/example/transform/emissions_electricity_sector/intermediate_emissions_electricity_sector_2016_17.sql
@@ -7,9 +7,9 @@ select
     SAFE_CAST(electricity_production__gj_ AS INT64) as production_in_gj,
     SAFE_CAST(electricity_production__mwh_ AS INT64) as production_in_mwh,
 
-    SAFE_CAST(total_scope_1_emissions__t_co2_e_ AS INT64) as scope_1_in_t_co2_e,
-    SAFE_CAST(total_scope_2_emissions__t_co2_e_2 AS INT64) as scope_2_in_t_co2_e,
-    SAFE_CAST(total_emissions__t_co2_e_ AS INT64) as total_emissions_in_t_co2_e,
+    SAFE_CAST(total_scope_1_emissions__t_co2e_ AS INT64) as scope_1_in_t_co2_e,
+    SAFE_CAST(total_scope_2_emissions__t_co2e_2 AS INT64) as scope_2_in_t_co2_e,
+    SAFE_CAST(total_emissions__t_co2e_ AS INT64) as total_emissions_in_t_co2_e,
 
     SAFE_CAST(emission_intensity__t_mwh_ AS FLOAT64) as emission_intensity_in_t_co2_e_mwh,
     
